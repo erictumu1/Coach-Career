a500f783430ca5ec49e28adbb355c49b469c1a11
erictumu1 <erictumu@outlook.com>
2025-06-17 11:20:23 -0400

    .

--- components/CarAnimation.jsx
+++ components/CarAnimation.jsx
@@ -1,24 +1,13 @@
 'use client';
 
 import { Player } from '@lottiefiles/react-lottie-player';
-import { useEffect, useState } from 'react';
 
 export default function CarAnimation() {
-  const [animationData, setAnimationData] = useState(null);
-
-  useEffect(() => {
-    fetch('/cardriving.json')
-      .then(res => res.json())
-      .then(data => setAnimationData(data));
-  }, []);
-
-  if (!animationData) return null;
-
   return (
     <Player
       autoplay
       loop
-      src={animationData}
+      src="/cardriving.json"
       style={{ height: '300px', width: '300px' }}
     />
   );
